b28b41e86f30f38d7e808583f20b7d9ade28063c
messa568 <messa1@hotmail.co.uk>
2018-01-27 20:06:28 +0000

    link to chat

--- src/app/job-match/job-match.component.ts
+++ src/app/job-match/job-match.component.ts
@@ -8,29 +8,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./job-match.component.scss']
 })
 export class JobMatchComponent {
+  history = [];
+  qAndA = {q:null, a: null};
   previousQuestion = '';
   previousAnswer = '';
   currentQuestion = "What do you want to change in your life?";
   currentAnswer: string;
   result: string;
   conversationStarted = false;
+  count = 1;
 
   constructor(private http: HttpClient) {
   }
 
   onFormSubmit(): void {
     let answ: JSON;
-    let obj: any =  {
-      "q":this.currentAnswer
+    let obj: any = {
+      "q": this.currentAnswer
     };
     answ = <JSON>obj;
-    this.http.post("http://localhost:3030/api/question", answ, { headers: { 'Content-Type': 'application/json' } }).subscribe(
+    this.http.post("http://localhost:3030/api/question/" + this.count, answ, { headers: { 'Content-Type': 'application/json' } }).subscribe(
       data => {
         console.log(data);
         this.conversationStarted = true;
         this.previousQuestion = this.currentQuestion;
         this.previousAnswer = this.currentAnswer;
+        if (data["question"] != null) {
+          this.currentQuestion = data["question"];
+        }
+        else if (data["feedback"]) {
+          this.currentQuestion = data["feedback"];
+        }
         this.currentAnswer = '';
+        this.count = this.count + 1;
       },
       err => {
         console.log("Error occured.")
--- src/app/shared/widget/widget.component.ts
+++ src/app/shared/widget/widget.component.ts
@@ -7,14 +7,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class WidgetComponent implements OnInit {
   @Input() bgClass: string;
+  @Input() key: string;
   @Input() icon: string;
   @Input() count: number;
   @Input() label: string;
   @Input() data: number;
   @Output() event: EventEmitter<any> = new EventEmitter();
+  isChat = false;
   constructor() { }
 
   ngOnInit() {
+    if (this.key === 'chat') {
+      this.isChat = true;
+    }
   }
-
 }
