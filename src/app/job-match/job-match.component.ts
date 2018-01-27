810600aa0e52f603dd781e1e5ecde762e5a6a30e
messa568 <messa1@hotmail.co.uk>
2018-01-27 17:16:38 +0000

    formatted, added previous questions

--- src/app/job-match/job-match.component.ts
+++ src/app/job-match/job-match.component.ts
@@ -8,16 +8,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./job-match.component.scss']
 })
 export class JobMatchComponent {
-  userAnswer: string;
+  previousQuestion = '';
+  previousAnswer = '';
+  currentQuestion = "What do you want to change in your life?";
+  currentAnswer: string;
   result: string;
+  conversationStarted = false;
 
   constructor(private http: HttpClient) {
   }
 
   onFormSubmit(): void {
-    this.http.post("http://localhost:3030/api/skills", this.userAnswer, { headers: { 'Content-Type': 'application/json' } }).subscribe(
+    let answ: JSON;
+    let obj: any =  {
+      "q":this.currentAnswer
+    };
+    answ = <JSON>obj;
+    this.http.post("http://localhost:3030/api/question", answ, { headers: { 'Content-Type': 'application/json' } }).subscribe(
       data => {
-        console.log("Job Match Result " + data);
+        console.log(data);
+        this.conversationStarted = true;
+        this.previousQuestion = this.currentQuestion;
+        this.previousAnswer = this.currentAnswer;
+        this.currentAnswer = '';
       },
       err => {
         console.log("Error occured.")
