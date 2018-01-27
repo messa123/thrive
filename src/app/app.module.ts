ac2164b2d952be84b6ff40cc78ade5dd92b040a0
messa568 <messa1@hotmail.co.uk>
2018-01-27 11:20:50 +0000

    homepage icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { WidgetComponent } from './shared/widget/widget.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomePageComponent
+    HomePageComponent,
+    WidgetComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/widget/widget.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-widget',
+  templateUrl: './widget.component.html',
+  styleUrls: ['./widget.component.scss']
+})
+export class WidgetComponent implements OnInit {
+  @Input() bgClass: string;
+  @Input() icon: string;
+  @Input() count: number;
+  @Input() label: string;
+  @Input() data: number;
+  @Output() event: EventEmitter<any> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
